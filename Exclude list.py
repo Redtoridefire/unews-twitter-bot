01c0e84dcddd0ff60dd02376490d3b8f5b63d27b
Redtoridefire <97312123+Redtoridefire@users.noreply.github.com>
2022-12-19 22:06:38 -0500

    Delete Exclude list.py

--- Exclude list.py
+++
@@ -1,9 +0,0 @@
-# Use the Google Search API to search for the title within the "example.com" domain
-# Replace "YOUR_GOOGLE_API_KEY" with your own API key
-params = {
-    'q': title,
-    'key': 'YOUR_GOOGLE_API_KEY',
-    'siteSearch': 'example.com',
-}
-r = requests.get('https://www.googleapis.com/customsearch/v1', params=params)
-results = r.json()['items']
